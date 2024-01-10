7fdfe295f2ae7d95a9d3c547e5513e83a24dd264
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-10 18:22:12 +0100

    playing around...

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>It works!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+    textDecorationLine: "underline",
+    fontSize: "20px",
   },
 });
